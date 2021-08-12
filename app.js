c15fc6b756115cd50d3f6af0626ad49a65a1be28
DushyantSpring <dushyant.singh@springrole.com>
2021-08-12 15:09:55 +0530

    folder structure and health route added

---
+++ app.js
@@ -0,0 +1,15 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const config = require('config');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const app = express();
+
+// view engine setup
+app.use(require('express-status-monitor')());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(cors());
+app.use('/', indexRouter);
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/health', (req, res) => {
+  return res.status(200).send({
+    message: 'OK'
+  });
+});
+
+router.use(function (req, res) {
+  let path = req._parsedUrl.pathname.toLowerCase();
+  let splittedPath = path.split('/');
+  let route = splittedPath[1];
+  let endPoint = splittedPath[2];
+  res.status(404).send({
+    message:
+      `Endpoint '${endPoint}' not found in ${route}.js in route folder, please declare it in '${route}.js' file and ` +
+      `also it in 'utils/validators/${route}.js' under ${req.method} protocol`,
+    status: 404,
+    note: `Make sure you have defined '${endPoint}' endpoint under ${req.method} protocol in the '${route}.js' route file`
+  });
+});
+
+module.exports = router;
